e226c34e01b7a6f02df342f946b5c4e54adc6d82
MaxCarlson <bart4282819@gmail.com>
2021-03-01 18:50:09 -0800

    Fixed a bug

--- kc-Means/kc_Means.py
+++ kc-Means/kc_Means.py
@@ -5,12 +5,12 @@ import random
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
+from scipy.spatial import Voronoi, voronoi_plot_2d
 
 class Base:
     def __init__(self, data, clusters, r):
         self.r = r
-        random.seed(1)
+        random.seed(2)
         self.data = data
         self.clusters = clusters
         self.minx = np.min(data[0])
@@ -20,7 +20,7 @@ class Base:
 
     def randomPoints(self):
         return [(random.uniform(self.minx, self.maxx), 
-            random.uniform(self.miny, self.maxy)) for _ in range(self.r)]
+            random.uniform(self.miny, self.maxy)) for _ in range(self.clusters)]
 
 
 class K_Means(Base):
@@ -33,7 +33,19 @@ class K_Means(Base):
             centroids, sses = self.run()
             if sses < best:
                 best = sses
-                bestCentroids = centroids
+                bestCentroids = np.array(centroids)[:,0,:]
+
+        fig, ax = plt.subplots()
+
+
+        vor = Voronoi(bestCentroids)
+        fig = voronoi_plot_2d(vor, ax=ax)
+        ax.scatter(self.data[:,0], self.data[:,1])
+        #plt.show()
+
+        ax.xaxis.zoom(-12)
+        ax.yaxis.zoom(-15)
+        plt.show()
 
     def run(self):
 
@@ -41,7 +53,7 @@ class K_Means(Base):
         prevBest = None
         bestCentroids = np.ones((len(self.data), 2)) * np.inf
 
-        i = 0
+        ii = 0
         while True:
             prevBest = bestCentroids
             bestCentroids = np.ones((len(self.data), 2)) * np.inf
@@ -60,21 +72,17 @@ class K_Means(Base):
                 break
 
             # Update 
-            notDone = False
             for i, c in enumerate(centroids): 
 
                 # Get data values belonging to centroid i
                 m = np.where(bestCentroids[:,0] == i)
                 x = self.data[m,:]
 
-                if notDone and prevWheres[i] != x:
-                    notDone = True
-                
                 # Update the centroid
-                centroids[i] = 1/len(bestCentroids[:,0]) * np.sum(x, 1)
+                centroids[i] = 1/(np.shape(x)[1] if np.shape(x)[1] else 1) * np.sum(x, 1)
                 
                 vv = 0
-            i += 1
+            ii += 1
 
         return centroids, np.sum(bestCentroids[:,1])
 
@@ -83,22 +91,11 @@ class K_Means(Base):
 
 
 
-
-                
-
-
-
-
 class C_Means(Base):
     def __init__(self, data, c, r):
         super().__init__(data, c, r)
 
 
-
-
-
-
-
 data = np.genfromtxt('545_cluster_dataset.txt')
 
-k = K_Means(data, 3, 2)
+k = K_Means(data, 5, 15)
